4766682df28997fb60a45ad748286b18524c429c
David Sheard <david.sheard@hotmail.com>
2020-01-24 10:35:36 +0000

    create backups of boilerplate App css and js, import component from react, make our App component stateful and give it a HW

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "ki",
+    };
+  }
+
+  render() {
+    const {type} = this.state;
+    return (
+      <div className={type} id="ep-interface">
+        <div id="ep-interface-inner">
+          <h1>I am a kitchen interface</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
