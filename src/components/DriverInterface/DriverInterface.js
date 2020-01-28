92654dd70f2a0375e1eedd6bb3af525933117b7d
David Sheard <david.sheard@hotmail.com>
2020-01-28 11:45:26 +0000

    convert KitchenInterface & DriverInterface into class components and init state

--- src/components/DriverInterface/DriverInterface.js
+++ src/components/DriverInterface/DriverInterface.js
@@ -4,22 +4,34 @@ import axios from 'axios';
 import classNames from 'classnames';
 import './DriverInterface.css';
 
-const DriverInterface = ({
-	type,
-	handle,
-	nonce,
-	store,
-	orders,
-}) =>
-  <div className={type} id="ep-interface">
-    <div id="ep-interface-inner">
-      <h1>I am the driver interface for {handle}</h1>
-      <p>{type}</p>
-      <p>{handle}</p>
-      <p>{nonce}</p>
-      <p>{store.wait_time}</p>
-      <p>{store.wt_updated}</p>
-    </div>
-  </div>
+class DriverInterface extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: props.orders,
+      store: props.store,
+    };
+  }
+
+  render() {
+    const {
+      store,
+      orders,
+    } = this.state;
+    const {
+      type,
+      handle,
+      nonce,
+      children,
+    } = this.props;
+    return (
+      <div className={type} id="ep-interface">
+        <div id="ep-interface-inner">
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default DriverInterface;
--- src/components/KitchenInterface/KitchenInterface.js
+++ src/components/KitchenInterface/KitchenInterface.js
@@ -4,22 +4,38 @@ import axios from 'axios';
 import classNames from 'classnames';
 import './KitchenInterface.css';
 
-const KitchenInterface = ({
-	type,
-	handle,
-	nonce,
-	store,
-	orders,
-}) =>
-  <div className={type} id="ep-interface">
-    <div id="ep-interface-inner">
-      <h1>I am the kitchen interface for {handle}</h1>
-      <p>{type}</p>
-      <p>{handle}</p>
-      <p>{nonce}</p>
-      <p>{store.wait_time}</p>
-      <p>{store.wt_updated}</p>
-    </div>
-  </div>
+class KitchenInterface extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: props.orders,
+      store: props.store,
+    };
+  }
+
+  render() {
+    const {
+      store,
+      orders,
+    } = this.state;
+    const {
+      type,
+      handle,
+      nonce,
+    } = this.props;
+    return (
+      <div className={type} id="ep-interface">
+        <div id="ep-interface-inner">
+          <h1>I am the kitchen interface for {handle}</h1>
+          <p>{type}</p>
+          <p>{handle}</p>
+          <p>{nonce}</p>
+          <p>{store.wait_time}</p>
+          <p>{store.wt_updated}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default KitchenInterface;
