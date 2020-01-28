5b9b4d6dac108220a9c3e1116ceba97b7e9ae563
David Sheard <david.sheard@hotmail.com>
2020-01-28 13:16:58 +0000

    remove property output from KitchenInterface and replace with children

--- src/components/KitchenInterface/KitchenInterface.js
+++ src/components/KitchenInterface/KitchenInterface.js
@@ -22,16 +22,12 @@ class KitchenInterface extends Component {
       type,
       handle,
       nonce,
+      children,
     } = this.props;
     return (
       <div className={type} id="ep-interface">
         <div id="ep-interface-inner">
-          <h1>I am the kitchen interface for {handle}</h1>
-          <p>{type}</p>
-          <p>{handle}</p>
-          <p>{nonce}</p>
-          <p>{store.wait_time}</p>
-          <p>{store.wt_updated}</p>
+          {children}
         </div>
       </div>
     );
