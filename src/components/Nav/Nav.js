126a6b720765a62511f9bfec076789a95607d4d9
David Sheard <david.sheard@hotmail.com>
2020-01-28 16:04:27 +0000

    switch all Nav buttons over to Button components, including those that were previously spans

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import classNames from 'classnames';
+import Button from '../Button'
 import './Nav.css';
-
-/**
- * Break this down into composite button components and a separate wait time container to be
- * rendered conditionally if props.type === "ki"
- */
 
 const Nav = (props) => {
   return (
@@ -15,17 +11,17 @@ const Nav = (props) => {
       {
         props.type === "ki" ?
         <nav>
-          <span id='int-nav-close' className='bg-grey'><span className='fas fa-times'></span></span>
-          <span id='int-nav-refresh' className='bg-grey'><span className='fas fa-undo'></span></span>
-          <span className='bg-grey bg-dark-grey' id='int-nav-filter-open'>OPEN</span>
-          <span className='bg-grey' id='int-nav-filter-other'>DONE</span>
-          <span id='int-nav-menu' className='bg-grey'>MENU</span>
+          <Button id='int-nav-close' className='bg-grey'><span className='fas fa-times'></span></Button>
+          <Button id='int-nav-refresh' className='bg-grey'><span className='fas fa-undo'></span></Button>
+          <Button className='bg-grey bg-dark-grey' id='int-nav-filter-open'>OPEN</Button>
+          <Button className='bg-grey' id='int-nav-filter-other'>DONE</Button>
+          <Button id='int-nav-menu' className='bg-grey'>MENU</Button>
           <span id='int-nav-wait' className='bg-grey wait-array'>
-            <button className='wait' value='0'>+0</button>
-            <button className='wait' value='20'>+20</button>
-            <button className='wait' value='30'>+30</button>
-            <button className='wait' value='40'>+40</button>
-            <button className='wait' value='60'>+60</button>
+            <Button className='wait' value='0'>+0</Button>
+            <Button className='wait' value='20'>+20</Button>
+            <Button className='wait' value='30'>+30</Button>
+            <Button className='wait' value='40'>+40</Button>
+            <Button className='wait' value='60'>+60</Button>
             <span className='wait-time'>
               <span className='inline-title'>Current Wait Time: </span>
               <span className='current-wait-time'>+0</span>
@@ -34,10 +30,10 @@ const Nav = (props) => {
           </nav>
         :
         <nav>
-          <span id="int-nav-close" className="bg-grey"><span className="fas fa-times"></span></span>
-          <span className="bg-grey bg-dark-grey" id="int-nav-filter-open">OPEN</span>
-          <span className="bg-grey" id="int-nav-filter-other">PAID</span>
-          <span className="bg-grey" id="int-nav-settle"><span className="fas fa-cash-register"></span></span>
+          <Button id="int-nav-close" className="bg-grey"><span className="fas fa-times"></span></Button>
+          <Button className="bg-grey bg-dark-grey" id="int-nav-filter-open">OPEN</Button>
+          <Button className="bg-grey" id="int-nav-filter-other">PAID</Button>
+          <Button className="bg-grey" id="int-nav-settle"><span className="fas fa-cash-register"></span></Button>
         </nav>
       }
     </div>
