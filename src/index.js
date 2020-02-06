4ec94011f6a672d05e691594472fb41332badc1f
David Sheard <david.sheard@hotmail.com>
2020-02-06 15:52:25 +0000

    render App component on button click, based on button's given type and store params

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import {ep, ajaxurl} from './constants';
 
-ReactDOM.render(<App
-	type="ki"
-	handle={ep.stores.testing.handle}
-	nonce={ep.staff_nonce}
-	wait_time={ep.stores.testing.wait_time}
-	wt_updated={ep.stores.testing.wt_updated}
-	ajaxurl={ajaxurl}
-	/>,
-	document.getElementById('root'));
+const root = document.getElementById("root");
+
+Array.from(document.getElementById("interface-btns-container")
+	.getElementsByClassName("submit-button-target"))
+	.forEach(i => {
+		i.addEventListener("click", e => {
+			const type = e.target.getAttribute("data-type");
+			const store = ep.stores[e.target.getAttribute("data-store")];
+			ReactDOM.render(<App
+				type={type}
+				handle={ep.stores.testing.handle}
+				nonce={ep.staff_nonce}
+				wait_time={ep.stores.testing.wait_time}
+				wt_updated={ep.stores.testing.wt_updated}
+				ajaxurl={ajaxurl}
+				/>,
+				root);
+			root.classList.add("open");
+		});
+	});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
