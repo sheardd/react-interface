358898b8e01ac57be38212e72730eecfee68986e
David Sheard <david.sheard@hotmail.com>
2020-02-11 12:51:05 +0000

    define ep and ajaxurl based on window variables instead of placeholder constants

--- src/index.js
+++ src/index.js
@@ -3,29 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import {ep, ajaxurl} from './constants';
 
 const root = document.getElementById("root");
+const ep = window.ep || null;
+const ajaxurl = window.ajaxurl || null;
 
-Array.from(document.getElementById("interface-btns-container")
-	.getElementsByClassName("submit-button-target"))
-	.forEach(i => {
-		i.addEventListener("click", e => {
-			const type = e.target.getAttribute("data-type");
-			const store = ep.stores[e.target.getAttribute("data-store")];
-			ReactDOM.render(<App
-				type={type}
-				handle={ep.stores.testing.handle}
-				nonce={ep.staff_nonce}
-				wait_time={ep.stores.testing.wait_time}
-				wt_updated={ep.stores.testing.wt_updated}
-				ajaxurl={ajaxurl}
-				/>,
-				root);
-			root.classList.add("open");
+if (ep && ajaxurl) {
+	Array.from(document.getElementById("interface-btns-container")
+		.getElementsByClassName("submit-button-target"))
+		.forEach(i => {
+			i.addEventListener("click", e => {
+				const type = e.target.getAttribute("data-type");
+				const store = ep.stores[e.target.getAttribute("data-store")];
+				ReactDOM.render(<App
+					type={type}
+					handle={store.handle}
+					nonce={ep.staff_nonce}
+					wait_time={ep.stores.testing.wait_time}
+					wt_updated={ep.stores.testing.wt_updated}
+					ajaxurl={ajaxurl}
+					/>,
+					root);
+				root.classList.add("open");
+			});
 		});
-	});
-
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
